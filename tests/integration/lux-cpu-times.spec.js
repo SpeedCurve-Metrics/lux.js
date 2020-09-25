5bcbdc28f5969fc4534bd530900d578de88d3159
Joseph Wynn <joseph@wildlyinaccurate.com>
2020-09-25 12:36:27 +1200

    Long task should always be >=50ms

--- tests/integration/lux-cpu-times.spec.js
+++ tests/integration/lux-cpu-times.spec.js
@@ -5,11 +5,10 @@ describe("LUX CPU timing", () => {
     await page.goto("http://localhost:3000/default-with-cpu.html", { waitUntil: "networkidle0" });
     const luxRequests = requestInterceptor.findMatchingRequests("https://lux.speedcurve.com/lux/");
     const beacon = new URL(luxRequests[0].url());
-
     const cpuMetrics = extractCpuMetrics(beacon.searchParams.get("CPU"));
 
     expect(cpuMetrics.n).toBe(1);
-    expect(cpuMetrics.s).toBeGreaterThan(1);
+    expect(cpuMetrics.s).toBeGreaterThan(49);
 
     // The test page should have one long task, so the median should equal the total
     expect(cpuMetrics.d).toEqual(cpuMetrics.s);
