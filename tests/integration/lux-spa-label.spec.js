303729b2d4a5caf65fb192e5ace200462c5de7d9
Joseph Wynn <joseph@wildlyinaccurate.com>
2020-10-06 18:38:23 +1300

    Add test for page labels in a SPA

---
+++ tests/integration/lux-spa-label.spec.js
@@ -0,0 +1,19 @@
+test("page label can be changed between SPA page loads", async () => {
+  let luxRequests
+  let beacon
+
+  await navigateTo("http://localhost:3000/auto-false.html");
+  await page.evaluate("LUX.send()");
+
+  luxRequests = requestInterceptor.findMatchingRequests("https://lux.speedcurve.com/lux/");
+  beacon = new URL(luxRequests[0].url());
+  expect(beacon.searchParams.get("l")).toBe("LUX SPA Test");
+
+  await page.evaluate("LUX.label = 'Custom Label'");
+  await page.evaluate("LUX.init()");
+  await page.evaluate("LUX.send()");
+
+  luxRequests = requestInterceptor.findMatchingRequests("https://lux.speedcurve.com/lux/");
+  beacon = new URL(luxRequests[1].url());
+  expect(beacon.searchParams.get("l")).toBe("Custom Label");
+});
