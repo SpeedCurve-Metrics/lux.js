13914f7eb734c4b0140f677b53e4a075a3254a96
Joseph Wynn <joseph@wildlyinaccurate.com>
2021-05-03 10:06:02 +1200

    Run prettier

--- tests/integration/lux-js-page-label.spec.js
+++ tests/integration/lux-js-page-label.spec.js
@@ -82,7 +82,7 @@ describe("LUX JS page label", () => {
     expect(luxRequests.getUrl(0).searchParams.get("l")).toEqual("LUX SPA Test With JS Page Label");
   });
 
-  test("internal LUX variables can't be accessed", async() => {
+  test("internal LUX variables can't be accessed", async () => {
     await page.evaluate("LUX.init()");
     await page.evaluate("LUX.jspagelabel = '_getPageLabel.toString()'");
     await page.evaluate("LUX.send()");
