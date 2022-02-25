f91f9352a42a3b0e8d4765fd67843f2dc48fec60
Joseph Wynn <joseph@wildlyinaccurate.com>
2022-02-25 17:00:17 +1300

    Add a debug log parser

---
+++ docs/debug-parser.ts
@@ -0,0 +1,130 @@
+import { LogEvent, LogEventRecord } from "../src/logger";
+
+const input = document.querySelector("#input") as HTMLTextAreaElement;
+const output = document.querySelector("#output");
+const parseBtn = document.querySelector("#parse");
+
+if (!input || !output || !parseBtn) {
+  throw new Error("Cannot start debug parser.");
+}
+
+parseBtn.addEventListener("click", () => {
+  output.innerHTML = "";
+
+  let inputEvents = [];
+
+  try {
+    inputEvents = JSON.parse(input.value);
+  } catch (err) {
+    output.appendChild(li(`Could not parse input: ${err}`, "red"));
+  }
+
+  inputEvents.forEach((event: LogEventRecord) => {
+    const date = new Date(event[0]);
+    const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+    output.appendChild(li(`${dateString}: ${getMessageForEvent(event)}`));
+  });
+});
+
+function getMessageForEvent(event: LogEventRecord): string {
+  const eventName = Object.keys(LogEvent).find((k) => LogEvent[k] === event[1]);
+  const args = event[2] as any[];
+
+  // Fallback message is the raw event name and any arguments
+  let message = eventName || "Unknown Event";
+
+  if (args.length) {
+    message += ` (${args.join(", ")})`;
+  }
+
+  switch (event[1]) {
+    case LogEvent.EvaluationStart:
+      if (args.length) {
+        return `lux.js v${args[0]} initialising`;
+      }
+
+      return "lux.js initialising";
+
+    case LogEvent.EvaluationEnd:
+      return "lux.js finished initialising";
+
+    case LogEvent.InitCalled:
+      return "LUX.init()";
+
+    case LogEvent.MarkCalled:
+      return `LUX.mark(${args.join(", ")})`;
+
+    case LogEvent.MeasureCalled:
+      return `LUX.measure(${args.join(", ")})`;
+
+    case LogEvent.AddDataCalled:
+      return `LUX.addData(${args.join(", ")})`;
+
+    case LogEvent.SendCalled:
+      if (args.length && args[0]) {
+        return "User-initiated LUX.send()";
+      }
+
+      return "Self-initiated LUX.send()";
+
+    case LogEvent.ForceSampleCalled:
+      return "LUX.forceSample()";
+
+    case LogEvent.SessionIsSampled:
+      return `Sample rate is ${args[0]}%. This session is being sampled.`;
+
+      case LogEvent.SessionIsNotSampled:
+      return `Sample rate is ${args[0]}%. This session is not being sampled.`;
+
+    case LogEvent.MainBeaconSent:
+      return `Main beacon sent: ${args[0]}`;
+
+    case LogEvent.UserTimingBeaconSent:
+      return `Supplementary user timing beacon sent: ${args[0]}`;
+
+    case LogEvent.InteractionBeaconSent:
+      return `Supplementary user interaction beacon sent: ${args[0]}`;
+
+    case LogEvent.CustomDataBeaconSent:
+      return `Supplementary custom data beacon sent: ${args[0]}`;
+
+    case LogEvent.PerformanceObserverError:
+      return `Error while initialising PerformanceObserver: ${args[0]}`;
+
+    case LogEvent.InputEventPermissionError:
+      return "Error reading input event. Cannot calculate FID for this page.";
+
+    case LogEvent.InnerHtmlAccessError:
+      return "Cannot read the innerHTML property of an element. Cannot calculate inline style or script sizes for this page.";
+
+    case LogEvent.EventTargetAccessError:
+      return "Error reading input event. Cannot calculate user interaction times for this page.";
+
+    case LogEvent.CookieReadError:
+      return "Error reading session cookie. This page will not be linked to a user session.";
+
+    case LogEvent.CookieSetError:
+      return "Error setting session cookie. This page will not be linked to a user session.";
+
+    case LogEvent.PageLabelEvaluationError:
+      return `Error while evaluating '${args[0]}' for the page label: ${args[1]}`;
+
+    case LogEvent.NavTimingNotSupported:
+      return "The Navigation Timing API is not supported. Performance metrics for this page will be limited.";
+
+    case LogEvent.PaintTimingNotSupported:
+      return "Start render time could not be determined.";
+  }
+
+  return message;
+}
+
+function li(textContent: string, className?: string): HTMLLIElement {
+  const el = document.createElement("li");
+
+  el.textContent = textContent;
+  el.className = className || "";
+
+  return el;
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,37 @@
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/lux.ts",
-  output: [
-    {
-      file: "dist/lux.js",
-      format: "iife",
-    },
-    {
-      file: "dist/lux.min.js",
+export default [
+  {
+    input: "src/lux.ts",
+    output: [
+      {
+        file: "dist/lux.js",
+        format: "iife",
+      },
+      {
+        file: "dist/lux.min.js",
+        format: "iife",
+        plugins: [terser()],
+      },
+    ],
+    plugins: [
+      typescript({
+        include: "src/**",
+      }),
+    ],
+  },
+  {
+    input: "docs/debug-parser.ts",
+    output: {
+      file: "docs/debug-parser.js",
       format: "iife",
       plugins: [terser()],
     },
-  ],
-  plugins: [
-    typescript({
-      include: "src/**",
-    }),
-  ],
-};
+    plugins: [
+      typescript({
+        include: ["docs/**", "src/**"],
+      }),
+    ],
+  },
+];
