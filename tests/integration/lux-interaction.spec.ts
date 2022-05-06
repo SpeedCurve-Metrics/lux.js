fac3bbc03aeff739127949d6647929534e6f78fa
Joseph Wynn <joseph@wildlyinaccurate.com>
2022-05-06 16:30:40 +1200

    Add test for keypress interaction

--- tests/integration/lux-interaction.spec.ts
+++ tests/integration/lux-interaction.spec.ts
@@ -1,47 +1,66 @@
-import { parseNestedPairs } from "../helpers/lux";
+import { getElapsedMs, parseNestedPairs } from "../helpers/lux";
 
 describe("LUX interaction", () => {
-  describe("in auto mode", () => {
-    let luxRequests;
-    let ixBeacon;
+  test("click interaction metrics are gathered", async () => {
+    const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
+    await navigateTo("/interaction.html");
+    const timeBeforeClick = await getElapsedMs(page);
+    await page.click("#button-with-id");
 
-    beforeAll(async () => {
-      await navigateTo("/interaction.html");
-      await page.click("#button-with-id");
+    const ixBeacon = luxRequests.getUrl(1);
+    const ixMetrics = parseNestedPairs(ixBeacon.searchParams.get("IX"));
 
-      luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
-      ixBeacon = luxRequests.getUrl(1);
-    });
+    // Separate request for main beacon and interaction beacon
+    expect(luxRequests.count()).toEqual(2);
 
-    test("interaction metrics are gathered", () => {
-      const ixMetrics = parseNestedPairs(ixBeacon.searchParams.get("IX"));
+    // Click time
+    expect(parseInt(ixMetrics.c)).toBeGreaterThanOrEqual(timeBeforeClick);
 
-      // Separate request for main beacon and interaction beacon
-      expect(luxRequests.count()).toEqual(2);
+    // Click attribution
+    expect(ixMetrics.ci).toEqual("button-with-id");
 
-      // Click time
-      expect(parseInt(ixMetrics.c)).toBeGreaterThan(1);
+    // Click coordinates
+    expect(parseInt(ixMetrics.cx)).toBeGreaterThan(0);
+    expect(parseInt(ixMetrics.cy)).toBeGreaterThan(0);
 
-      // Click attribution
-      expect(ixMetrics.ci).toEqual("button-with-id");
+    // FID
+    expect(parseInt(ixBeacon.searchParams.get("FID"))).toBeGreaterThan(0);
+  });
 
-      // Click coordinates
-      expect(parseInt(ixMetrics.cx)).toBeGreaterThan(0);
-      expect(parseInt(ixMetrics.cy)).toBeGreaterThan(0);
+  test("keypress interaction metrics are gathered", async () => {
+    const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
+    await navigateTo("/interaction.html");
+    const timeBeforeKeyPress = await getElapsedMs(page);
+    const button = await page.$("#button-with-id");
+    await button.press("Enter");
 
-      // FID
-      expect(parseInt(ixBeacon.searchParams.get("FID"))).toBeGreaterThan(0);
-    });
+    const ixBeacon = luxRequests.getUrl(1);
+    const ixMetrics = parseNestedPairs(ixBeacon.searchParams.get("IX"));
 
-    test("only high level metrics are sent in the interaction beacon", () => {
-      expect(ixBeacon.searchParams.get("HN")).toEqual("localhost");
-      expect(ixBeacon.searchParams.get("PN")).toEqual("/interaction.html");
+    // Key press time
+    expect(parseInt(ixMetrics.k)).toBeGreaterThanOrEqual(timeBeforeKeyPress);
 
-      // Pathname should be the last query parameter
-      const lastQueryParam = [...ixBeacon.searchParams.entries()].pop();
+    // Key press attribution
+    expect(ixMetrics.ki).toEqual("button-with-id");
 
-      expect(lastQueryParam).toEqual(["PN", "/interaction.html"]);
-    });
+    // FID
+    expect(parseInt(ixBeacon.searchParams.get("FID"))).toBeGreaterThan(0);
+  });
+
+  test("only high level metrics are sent in the interaction beacon", async () => {
+    const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
+    await navigateTo("/interaction.html");
+    await page.click("#button-with-id");
+
+    const ixBeacon = luxRequests.getUrl(1);
+
+    expect(ixBeacon.searchParams.get("HN")).toEqual("localhost");
+    expect(ixBeacon.searchParams.get("PN")).toEqual("/interaction.html");
+
+    // Pathname should be the last query parameter
+    const lastQueryParam = [...ixBeacon.searchParams.entries()].pop();
+
+    expect(lastQueryParam).toEqual(["PN", "/interaction.html"]);
   });
 
   test("gather IX metrics in a SPA", async () => {
