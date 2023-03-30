dd48685412ba22ada0e0ee3a823153d048c78878
Joseph Wynn <joseph@speedcurve.com>
2023-03-30 15:31:18 +1300

    Add more tests for CLS

---
+++ src/metric/CLS.test.ts
@@ -0,0 +1,35 @@
+import * as CLS from "./CLS";
+
+describe("CLS", () => {
+  test("CLS is windowed", () => {
+    CLS.addEntry(makeEntry(100, 0.01));
+    CLS.addEntry(makeEntry(120, 0.01));
+    CLS.addEntry(makeEntry(1000, 0.01));
+    expect(CLS.getCLS()).toEqual(0.03);
+
+    // New window because it was >= 1000ms since the last entry
+    CLS.addEntry(makeEntry(2000, 0.01));
+    expect(CLS.getCLS()).toEqual(0.01);
+
+    CLS.addEntry(makeEntry(2800, 0.01));
+    CLS.addEntry(makeEntry(3600, 0.01));
+    CLS.addEntry(makeEntry(4400, 0.01));
+    CLS.addEntry(makeEntry(5200, 0.01));
+    CLS.addEntry(makeEntry(6000, 0.01));
+    CLS.addEntry(makeEntry(6800, 0.01));
+
+    // New window because despite all the entries being 800ms apart, the first entry in the current
+    // window (at 2000ms) was >= 5000ms ago
+    CLS.addEntry(makeEntry(7600, 0.01));
+    CLS.addEntry(makeEntry(8400, 0.01));
+    expect(CLS.getCLS()).toEqual(0.02);
+  });
+});
+
+function makeEntry(startTime: number, value: number, hadRecentInput = false): LayoutShift {
+  return {
+    startTime,
+    value,
+    hadRecentInput,
+  } as LayoutShift;
+}
--- tests/integration/lux-layout-shifts.spec.ts
+++ tests/integration/lux-layout-shifts.spec.ts
@@ -1,20 +1,27 @@
 describe("LUX layout shifts", () => {
-  const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
-
   test("CLS is measured", async () => {
+    const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
     await navigateTo("/layout-shifts.html");
 
     const beacon = luxRequests.getUrl(0);
 
-    console.log(parseFloat(beacon.searchParams.get("CLS")));
     expect(parseFloat(beacon.searchParams.get("CLS"))).toBeGreaterThan(0);
   });
 
-  test("CLS is windowed into 5-second sessions", async () => {
-    await navigateTo("/layout-shifts.html?injectScript=LUX.auto=false;");
+  test("CLS is reset between SPA page transitions", async () => {
+    const luxRequests = requestInterceptor.createRequestMatcher("/beacon/");
+    await navigateTo("/layout-shifts.html?&injectScript=LUX.auto=false;");
+    await waitForNetworkIdle();
+    await page.evaluate("LUX.send()");
 
-    const beacon = luxRequests.getUrl(0);
-
+    let beacon = luxRequests.getUrl(0);
     expect(parseFloat(beacon.searchParams.get("CLS"))).toBeGreaterThan(0);
+
+    await page.evaluate("LUX.init()");
+    await page.waitForTimeout(200);
+    await page.evaluate("LUX.send()");
+
+    beacon = luxRequests.getUrl(1);
+    expect(parseFloat(beacon.searchParams.get("CLS"))).toEqual(0);
   });
 });
