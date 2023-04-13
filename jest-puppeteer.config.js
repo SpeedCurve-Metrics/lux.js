3456d099df1ea5208de94164a44f5789f75f3f26
Joseph Wynn <joseph@speedcurve.com>
2023-04-13 15:36:03 +1200

    Allow headless mode to be disabled with HEADLESS=false

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   exitOnPageError: false,
+  launch: {
+    headless: process.env.HEADLESS !== "false",
+  },
   server: {
     command: "node tests/server.js > server.log",
     port: 3000,
