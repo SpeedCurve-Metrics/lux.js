4c3b7834cc4121cbb7eed8e8e4f246e47145088e
Joseph Wynn <joseph@speedcurve.com>
2023-06-01 16:11:49 -0700

    Use the maximum session value for CLS, not the latest session value

--- tests/unit/CLS.test.ts
+++ tests/unit/CLS.test.ts
@@ -3,27 +3,31 @@ import * as CLS from "../../src/metric/CLS";
 
 describe("CLS", () => {
   test("CLS is windowed", () => {
-    CLS.addEntry(makeEntry(100, 0.01));
-    CLS.addEntry(makeEntry(120, 0.01));
-    CLS.addEntry(makeEntry(1000, 0.01));
-    expect(CLS.getCLS()).toEqual(0.03);
+    CLS.addEntry(makeEntry(100, 0.1));
+    CLS.addEntry(makeEntry(120, 0.2));
+    CLS.addEntry(makeEntry(1000, 0.1));
+    expect(CLS.getCLS()).toBeCloseTo(0.4);
 
     // New window because it was >= 1000ms since the last entry
-    CLS.addEntry(makeEntry(2000, 0.01));
-    expect(CLS.getCLS()).toEqual(0.01);
+    CLS.addEntry(makeEntry(2000, 0.5));
+    expect(CLS.getCLS()).toBeCloseTo(0.5);
 
-    CLS.addEntry(makeEntry(2800, 0.01));
-    CLS.addEntry(makeEntry(3600, 0.01));
-    CLS.addEntry(makeEntry(4400, 0.01));
-    CLS.addEntry(makeEntry(5200, 0.01));
-    CLS.addEntry(makeEntry(6000, 0.01));
-    CLS.addEntry(makeEntry(6800, 0.01));
+    // Same window
+    CLS.addEntry(makeEntry(2800, 0.1));
+    CLS.addEntry(makeEntry(3600, 0.1));
+    CLS.addEntry(makeEntry(4400, 0.1));
+    CLS.addEntry(makeEntry(5200, 0.1));
+    CLS.addEntry(makeEntry(6000, 0.1));
+    CLS.addEntry(makeEntry(6800, 0.1));
+    expect(CLS.getCLS()).toBeCloseTo(1.1);
 
     // New window because despite all the entries being 800ms apart, the first entry in the current
     // window (at 2000ms) was >= 5000ms ago
-    CLS.addEntry(makeEntry(7600, 0.01));
-    CLS.addEntry(makeEntry(8400, 0.01));
-    expect(CLS.getCLS()).toEqual(0.02);
+    CLS.addEntry(makeEntry(7600, 0.3));
+    CLS.addEntry(makeEntry(8400, 0.1));
+
+    // Score didn't change because the biggest window was still the previous window
+    expect(CLS.getCLS()).toBeCloseTo(1.1);
   });
 });
 
