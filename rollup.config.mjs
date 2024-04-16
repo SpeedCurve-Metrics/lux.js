a07b5326e44111f6cbe3cd0bd6b00a8489d004a5
Joseph Wynn <joseph@speedcurve.com>
2024-04-17 11:38:58 +1200

    Fix no-polyfill build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,14 +2,14 @@ import replace from "@rollup/plugin-replace";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
-const minifiedLux = (file, polyfills) => ({
+const outputConfig = (file, polyfills, minified) => ({
   file,
   format: "iife",
   plugins: [
     replace({
       __ENABLE_POLYFILLS: JSON.stringify(polyfills),
     }),
-    terser(),
+    minified ? terser() : undefined,
   ],
   sourcemap: true,
 });
@@ -18,20 +18,14 @@ export default [
   {
     input: "src/lux.ts",
     output: [
-      {
-        file: "dist/lux.js",
-        format: "iife",
-        sourcemap: true,
-      },
-      minifiedLux("dist/lux.min.js", true),
-      minifiedLux("dist/lux-no-polyfills.min.js", false),
+      outputConfig("dist/lux.js", true, false),
+      outputConfig("dist/lux.min.js", true, true),
+      outputConfig("dist/lux-no-polyfills.js", false, false),
+      outputConfig("dist/lux-no-polyfills.min.js", false, true),
     ],
     plugins: [
       typescript({
         include: "src/**",
-      }),
-      replace({
-        __ENABLE_POLYFILLS: JSON.stringify(true),
       }),
     ],
   },
