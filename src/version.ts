5c5b798061666fb936d1bd9b11778fef02e571dd
Joseph Wynn <joseph@speedcurve.com>
2024-06-27 14:58:10 +1200

    4.0.20

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 import { padStart } from "./string";
 
-export const VERSION = "4.0.19";
+export const VERSION = "4.0.20";
 
 /**
  * Returns the version of the script as a float to be stored in legacy systems that do not support
